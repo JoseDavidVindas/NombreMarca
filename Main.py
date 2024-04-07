b586937f2d6adcb4475cb8abd9bb9dc12264b92b
JoseDavidVindas <jose.chavarria34@ulatina.net>
2024-04-07 11:54:29 -0600

    Se sube el proyecto

---
+++ Main.py
@@ -0,0 +1,7 @@
+palabra1 = input("Por favor, ingrese una palabra: ")
+
+palabra2 = input("Ahora, ingrese otra palabra: ")
+
+marca_creativa = palabra1 + palabra2
+
+print("Su marca creativa es:", marca_creativa)
